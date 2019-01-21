7dc9d08ec36b4db2f6c65203d3d89bfec6b00e32
Jaden Balogh <jadenbalogh@gmail.com>
2019-01-20 17:00:05 -0800

    Updated Enemy movement
    Enemies now read their movement path from a text file. Updated EnemyAction classes to include Move, Idle and Spawn.

--- OpenProject2018/Assets/Scripts/Enemy.cs
+++ OpenProject2018/Assets/Scripts/Enemy.cs
@@ -1,121 +1,96 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Globalization;
 using System;
 
 public class Enemy : MonoBehaviour {
 
-    public TextAsset movementSequenceFile;
-
-    private EnemyAction[] actionSequence;
-    private int actionIndex;
-
     /// <summary>
-    /// Ordered list of points that the enemy will path along.
+    /// Text file containing a list of EnemyActions that form the enemy's movement sequence.
     /// </summary>
-    public Transform[] path;
+    public TextAsset movementSequenceFile;
     /// <summary>
     /// How close the player must be to a point on the path before being considered at that point.
     /// </summary>
     public float stopDistance = 0.01f;
-    /// <summary>
-    /// Velocity at which the enemy will travel between points on the path.
-    /// </summary>
-    public float movementSpeed = 1f;
 
     /// <summary>
-    /// Index of the current position in the list of points.
+    /// List of EnemyActions that form the enemy's movement sequence.
     /// </summary>
-    private int pathIndex = 0;
+    private List<EnemyAction> movementSequence;
+    /// <summary>
+    /// Index of the current step in the enemy's movement sequence.
+    /// </summary>
+    private int actionIndex;
 
-    /// <summary>
-    /// The last point on the path the enemy reached.
-    /// </summary>
-    private Vector3 prevPos;
-    /// <summary>
-    /// Point on the path the enemy is currently moving toward.
-    /// </summary>
-    private Vector3 targetPos;
-    /// <summary>
-    /// Interpolant used in Vector3.Lerp to smoothly move between points on the path. 
-    /// Measures the percentage of the distance travelled.
-    /// </summary>
-    private float lerpDistance;
-    /// <summary>
-    /// Total time required to move between two points on the path at the defined movementSpeed.
-    /// </summary>
-    private float lerpDuration = 0;
-    /// <summary>
-    /// Timer of how long the enemy has been interpolating between two points.
-    /// </summary>
-    private float lerpTimer = 0;
-    
-	private void Start ()
+    private void Start ()
     {
-        transform.position = path[0].position;
-        TargetNextPoint();
+        movementSequence = new List<EnemyAction>();
+        ReadMovementSequenceFile();
+
+        actionIndex = 0;
+        movementSequence[actionIndex].Init(gameObject);
     }
 	
 	private void FixedUpdate ()
     {
-        bool completed = actionSequence[actionIndex].Run(gameObject);
+        bool completed = movementSequence[actionIndex].Run(gameObject);
 
         if (completed)
         {
             actionIndex++;
-            actionSequence[actionIndex].Init(gameObject);
+
+            if (actionIndex >= movementSequence.Count)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            movementSequence[actionIndex].Init(gameObject);
         }
 	}
 
     private void ReadMovementSequenceFile()
     {
-        string[] sequence = movementSequenceFile.text.Split(' ');
+        string[] sequence = movementSequenceFile.text.Split(new string[] { Environment.NewLine, " " }, StringSplitOptions.None);
         for (int i = 0; i < sequence.Length; i++)
         {
             string word = sequence[i];
+            float x, y, t;
             switch (word)
             {
+                case "SPAWN":
+                    EnemySpawnAction spawn = new EnemySpawnAction();
+
+                    x = float.Parse(sequence[++i]);
+                    y = float.Parse(sequence[++i]);
+                    spawn.spawnPosition = new Vector3(x, y, 0);
+
+                    movementSequence.Add(spawn);
+                    break;
+
                 case "MOVE":
                     EnemyMovementAction move = new EnemyMovementAction();
-                    float x = float.Parse(sequence[++i]);
-                    float y = float.Parse(sequence[++i]);
+
+                    x = float.Parse(sequence[++i]);
+                    y = float.Parse(sequence[++i]);
                     move.targetPosition = new Vector3(x, y, 0);
-                    float t = float.Parse(sequence[++i]);
+
+                    t = float.Parse(sequence[++i]);
                     move.moveDuration = t;
+
+                    movementSequence.Add(move);
                     break;
+
                 case "IDLE":
-                    EnemyMovementAction idle = new EnemyMovementAction();
-                    float d = float.Parse(sequence[++i]);
-                    idle.moveDuration = d;
+                    EnemyIdleAction idle = new EnemyIdleAction();
+
+                    t = float.Parse(sequence[++i]);
+                    idle.idleDuration = t;
+
+                    movementSequence.Add(idle);
                     break;
             }
         }
-    }
-
-    private void Move()
-    {
-        lerpTimer += Time.deltaTime;
-
-        if (Vector3.Distance(targetPos, transform.position) < stopDistance)
-        {
-            lerpTimer = 0;
-            TargetNextPoint();
-        }
-
-        lerpDistance = lerpTimer / lerpDuration;
-        transform.position = Vector3.Lerp(prevPos, targetPos, lerpDistance);
-    }
-
-    private void TargetNextPoint()
-    {
-        pathIndex++;
-
-        if (pathIndex == path.Length)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        prevPos = path[pathIndex - 1].position;
-        targetPos = path[pathIndex].position;
-        lerpDuration = Vector3.Distance(prevPos, targetPos) / movementSpeed;
     }
 }
---
+++ OpenProject2018/Assets/Scripts/EnemyIdleAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyIdleAction : EnemyAction
+{
+    /// <summary>
+    /// Total time the enemy will stay idle.
+    /// </summary>
+    public float idleDuration;
+
+    /// <summary>
+    /// Timer of how long the enemy has been idle.
+    /// </summary>
+    private float idleTimer = 0;
+
+    public override void Init(GameObject enemy)
+    {
+        idleTimer = 0;
+    }
+
+    /// <summary>
+    /// Idle function. Returns true if enemy has been idle for the full duration.
+    /// </summary>
+    public override bool Run(GameObject enemy)
+    {
+        idleTimer += Time.deltaTime;
+        
+        if (idleTimer > idleDuration)
+        {
+            isCompleted = true;
+        }
+
+        return isCompleted;
+    }
+}
