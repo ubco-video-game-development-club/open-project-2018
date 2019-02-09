dce5cd9d452499e2de8dd0b00493a344f20b4438
Johnny Wu <46991587+snailispro@users.noreply.github.com>
2019-02-09 04:24:36 -0800

    Tweaked movement to make it more like Touhou; moved some line around for performance improvements
    RigidBody and Vector2 objects are now created in Start(); Input now returns integer values; Added "slow down" button
    
    I made this in a newer version of Unity. Should I downgrade this?

--- OpenProject2018/Assets/Scripts/Player.cs
+++ OpenProject2018/Assets/Scripts/Player.cs
@@ -7,12 +7,20 @@ public class Player : MonoBehaviour {
     /// </summary>
     [Header("Player Options")]
     public float movementSpeed = 1f;
+    public float acceleration = 0.2f;
+    public float damping = 0.8f;
     /// <summary>
     /// Minimum time delay between firing bullets.
     /// </summary>
     public float attackCooldown = 0;
     
     private float attackTimer = 0;
+
+    /// <summary>
+    /// Basic player stats
+    /// </summary>
+    public float health = 100f;
+    public float armor = 2f;
 
     /// <summary>
     /// Bullet prefab to be spawned by the player when firing bullets.
@@ -28,29 +36,55 @@ public class Player : MonoBehaviour {
     /// </summary>
     public float bulletSpawnDistance = 1f;
 
-	private void Start()
+    private Rigidbody2D rb;
+    private Vector2 movementVelocity;
+    private float xDir = 0f;
+    private float yDir = 0f;
+    private float speedModifier = 1f;
+
+    private void Start()
     {
-		
-	}
+        rb = GetComponent<Rigidbody2D>();   //Get rigidbody of player for movement
+    }
 	
 	private void FixedUpdate()
     {
         Move();
 
         attackTimer += Time.deltaTime;
+
+        //Prevent attackTimer from going too high
+        if(attackTimer > attackCooldown + 1)    
+        {
+            attackTimer = attackCooldown + 1;
+        }
+
         if (Input.GetButton("Fire1") && attackTimer > attackCooldown)
         {
             Shoot();
             attackTimer = 0;
         }
+
 	}
 
     private void Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        
-        GetComponent<Rigidbody2D>().velocity = new Vector2(x * movementSpeed, y * movementSpeed);
+        xDir = Input.GetAxisRaw("Horizontal");
+        yDir = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speedModifier = 0.5f;
+        }
+        else
+        {
+            speedModifier = 1f;
+        }
+
+        movementVelocity.x = xDir * movementSpeed * speedModifier;
+        movementVelocity.y = yDir * movementSpeed * speedModifier;
+
+        rb.velocity = movementVelocity;
     }
 
     private void Shoot()
